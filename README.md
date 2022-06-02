8e9ddc1d610bce3453e0ed5a4acedf16e71175e7
杜审言 <2645472474@qq.com>
2022-06-02 14:06:50 +0800

    fix

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 ## budaiyun-web
 
 
-
-
 > 考虑在开一分支vue-source 使用主流技术(vue3 + tailwindCSS + TS)
 
 
@@ -11,3 +9,14 @@
 [Less中文网](https://less.bootcss.com/)
 
 [视频地址(B站)](https://www.bilibili.com/video/BV117411n7R1?spm_id_from=333.999.0.0)
+
+### 工具库链接
+
+[Glide.js](https://glidejs.com/docs/) 
+
+[anime.js官网_免费、灵活的轻型JavaScript动画库](https://www.animejs.cn/) 
+
+[Isotope(同位素元素布局)](https://isotope.metafizzy.co/) 
+
+[ScrollReveal (scrollrevealjs.org)](https://scrollrevealjs.org/) 
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 // 轮播效果
-var glide = new Glide('.glide')
+const glide = new Glide('.glide')
+const captionsEl = document.querySelectorAll('.slide-caption')
+
+glide.on(['mount.after', 'run.after'], () => {
+  const caption = captionsEl[glide.index]
+  anime({
+    targets: caption.children,
+    opacity: [0, 1],
+    duration: 400,
+    easing: 'linear',
+    delay: anime.stagger(400, { start: 300 }),
+    translateY: [anime.stagger([40, 10]), 0]
+  })
+})
+
+glide.on(['run.before'],()=>{
+  document.querySelectorAll('.slide-caption > *').forEach(el=>{
+    el.getElementsByClassName.opacity = 0;
+  })
+})
 glide.mount()
 
 
 // 同位数排列
-new Isotope(".cases", {
+const isotope = new Isotope("#cases", {
   layoutMode: 'fitRows',
   itemSelector: '.case-item'
 })
@@ -12,7 +31,7 @@ new Isotope(".cases", {
 const filterBtns = document.querySelector(".filter-btns")
 console.log(filterBtns)
 filterBtns.addEventListener('click', e => {
-  const {target} = e;
+  const { target } = e;
   const filterOption = target.getAttribute('data-filter')
   if (filterOption) {
     document.querySelectorAll('.filter-btn.active').forEach(btn => btn.classList.remove('active'))
